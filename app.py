17c8271517466def88a58e9807cd9b9a6aa5cffe
Heiko <hfrosch@gmail.com>
2024-07-15 20:01:46 +0200

    Refactored code to modularize models into a separate file for better organization and maintainability.

--- app.py
+++ app.py
@@ -12,19 +12,15 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
-# Add bc of error
 import collections
 collections.Callable = collections.abc.Callable
-# Add Migrate 
 from flask_migrate import Migrate
-# Add to count shows
 from sqlalchemy import func
-# Add to get time
 from datetime import datetime
-# Importing SQLAlchemyError for handling database interaction errors
 from sqlalchemy.exc import SQLAlchemyError
-# Used for triggering HTTP errors
 from flask import abort 
+# Importiere Modelle
+from models import db, Venue, Artist, Show 
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -32,74 +28,9 @@ from flask import abort
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+db.init_app(app)
 
 migrate = Migrate(app, db)
-
-# TODO: connect to a local postgresql database >> done!
-
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate >> done!
-    genres = db.Column(db.ARRAY(db.String(120))) # To store multiple genres
-    website = db.Column(db.String(500)) # New field for the website link
-    seeking_talent = db.Column(db.Boolean, default=False) # New field for 'Seeking Talent'
-    seeking_description = db.Column(db.String(500)) # New field for 'Seeking Description'
-    
-    # Relationship with Artist model using Show model as secondary
-    # artists = db.relationship('Artist', secondary='Show', backref=db.backref('venues', lazy=True))
-
-    # Relationship with Show model. If a Venue is deleted, its associated Show instances are also deleted.
-    shows = db.relationship('Show', backref='venue_shows', lazy=True, cascade='all, delete-orphan', overlaps="artists,venues")
-
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate >> done!
-    genres = db.Column(db.ARRAY(db.String(120))) # To store multiple genres
-    website = db.Column(db.String(500)) # New field for the website link
-    seeking_venue = db.Column(db.Boolean, default=False) # New field for 'Looking for Venues'
-    seeking_description = db.Column(db.String(500)) # New field for 'Seeking Description'
-
-    # Relationship with Venue model using Show model as secondary
-    # venues = db.relationship('Venue', secondary='Show', backref=db.backref('artists', lazy=True))
-
-    shows = db.relationship('Show', backref='artist_shows', lazy=True, overlaps="venues")
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-class Show(db.Model):
-    __tablename__ = 'Show'
-    id = db.Column(db.Integer, primary_key=True)
-    start_time = db.Column(db.DateTime, nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)  # Foreign Key reference to Artist model
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)  # Foreign Key reference to Venue model
-    # Establish relationship with Artist and Venue models
-    artist = db.relationship('Artist', overlaps="artist_shows,shows")  # 'shows' relationship in Artist model
-    venue = db.relationship('Venue', overlaps="venue_shows,shows,venues")  # 'shows' relationship in Venue models
 
 #----------------------------------------------------------------------------#
 # Filters.
---
+++ models.py
@@ -0,0 +1,68 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate >> done!
+    genres = db.Column(db.ARRAY(db.String(120))) # To store multiple genres
+    website = db.Column(db.String(500)) # New field for the website link
+    seeking_talent = db.Column(db.Boolean, default=False) # New field for 'Seeking Talent'
+    seeking_description = db.Column(db.String(500)) # New field for 'Seeking Description'
+    
+    # Relationship with Artist model using Show model as secondary
+    # artists = db.relationship('Artist', secondary='Show', backref=db.backref('venues', lazy=True))
+
+    # Relationship with Show model. If a Venue is deleted, its associated Show instances are also deleted.
+    shows = db.relationship('Show', backref='venue_shows', lazy=True, cascade='all, delete-orphan', overlaps="artists,venues")
+
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate >> done!
+    genres = db.Column(db.ARRAY(db.String(120))) # To store multiple genres
+    website = db.Column(db.String(500)) # New field for the website link
+    seeking_venue = db.Column(db.Boolean, default=False) # New field for 'Looking for Venues'
+    seeking_description = db.Column(db.String(500)) # New field for 'Seeking Description'
+
+    # Relationship with Venue model using Show model as secondary
+    # venues = db.relationship('Venue', secondary='Show', backref=db.backref('artists', lazy=True))
+
+    shows = db.relationship('Show', backref='artist_shows', lazy=True, overlaps="venues")
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'Show'
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)  # Foreign Key reference to Artist model
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)  # Foreign Key reference to Venue model
+    # Establish relationship with Artist and Venue models
+    artist = db.relationship('Artist', overlaps="artist_shows,shows")  # 'shows' relationship in Artist model
+    venue = db.relationship('Venue', overlaps="venue_shows,shows,venues")  # 'shows' relationship in Venue models
